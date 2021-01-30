c97dcad56740c1788621508ef6e716c04f45e1e8
Tanviras <tanvirasme@gmail.com>
2021-01-31 00:53:57 +0600

    done Alhamdulillah

--- diffEquals.js
+++ diffEquals.js
@@ -1,7 +1,99 @@
-const num1=2;
-const num2="2";
+// const num1=2;
+// const num2="2";
 
-if(num1==num2){
+// if(num1==num2){//ekta number 2,arekta string 2, tobou true dekhacche == er karone. == doesn't check the type,only values are checked.
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+// const num1=2;
+// const num2="2";
+
+// if(num1===num2){//=== does check both of the type and values
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+
+
+
+//boolean true is equal to 1, false is 0
+// const num1=1;
+// const num2=true;
+
+// if(num1==num2){//== doesn't check the type,so it will be true
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+
+
+// const num1=1;
+// const num2=false;
+
+// if(num1==num2){//== doesn't check the type,so it will be false
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+//boolean false is equal to 0.
+// const num1=0;
+// const num2=false;
+
+// if(num1==num2){//== doesn't check the type,so it will be true
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+// const num1=1;
+// const num2=false;
+
+// if(num1==num2){//== doesn't check the type,so it will be false
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+// const num1=0;
+// const num2=false;
+
+// if(num1===num2){//=== does check both of the type & value,so it will be false
+//     console.log("True");
+// }
+// else{
+//     console.log("False");
+// }
+
+
+
+
+const num1=1;
+const num2=true;
+
+if(num1===num2){//=== does check both of the type and values,so it will be false
     console.log("True");
 }
 else{
