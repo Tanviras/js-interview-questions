ba882466c9399f4cc0657e47b7fd6feae55ae120
Tanviras <tanvirasme@gmail.com>
2021-01-30 22:56:33 +0600

    done Alhamdulillah

---
+++ TruthyAndFalsy.js
@@ -0,0 +1,130 @@
+//1
+//const age=4;
+
+//if(age>0){//Age specified kora ache. Tai khub-e normal je code etay dhukbe.
+    //console.log("Condition is true");
+//}
+//else{
+  //  console.log("condition is false");
+//}
+
+
+
+
+
+
+
+
+//2
+//const age=4;
+
+//if(age){// age specified kora ache. Tar mane je age-e deya thakuk na kn,otai neya hobe jodi age=0 na hoy.
+   // console.log("Condition is true");
+//}
+//else{
+   // console.log("condition is false");
+//}
+
+
+
+
+
+
+
+//3
+//const age=0;//age jokhon 0
+
+//if(age){//age specified kora nai. Tra mane age jetai deya ache,seta. But deya ache 0. Tai ei age neya hobe na.
+   // console.log("Condition is true");
+//}
+//else{//tai age pore jabe etar under a
+  //  console.log("condition is false");
+//}
+
+
+
+
+//4
+//Chaile string diyeo check kora jay
+//const name="Sulaiman";
+
+//if(name.length){
+    //console.log("True:String has letters");
+//}
+//else{
+    //console.log("False:string doesn't have letters");
+//}
+
+
+
+
+
+
+//5
+//const name="";
+
+//if(name.length){
+   // console.log("True:String has letters");
+//}
+//else{
+    //console.log("False:string doesn't have letters");
+//}
+
+//That means, any number is true unless it is 0. Any string is true unless it is empty(not even space)
+
+
+
+
+
+
+
+//6
+//let age;//no number as age is defined,this will work as False
+
+//if(age){
+   // console.log("True:String has letters");
+//}
+//else{
+  //  console.log("False:string doesn't have letters");
+//}
+
+
+
+
+
+//7
+//let age=null;//null is given,this will work as False
+
+//if(age){
+   // console.log("True:String has letters");
+//}
+//else{
+   // console.log("False:string doesn't have letters");
+//}
+
+
+
+
+//8
+//let age=false;//false is given,this will work as False
+
+//if(age){
+    //console.log("True:String has letters");
+//}
+//else{
+   // console.log("False:string doesn't have letters");
+//}
+
+
+
+
+
+//9
+let age=NaN;//NaN(not a number) is given,this will work as False
+
+if(age){
+    console.log("True:String has letters");
+}
+else{
+    console.log("False:string doesn't have letters");
+}
