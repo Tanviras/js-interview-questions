8ec7e30dcd643e2d6fa688d4d1b45ad92f0e86de
Tanviras <tanvirasme@gmail.com>
2021-01-31 00:26:38 +0600

    done Alhamdulillah

---
+++ diffEquals.js
@@ -0,0 +1,9 @@
+const num1=2;
+const num2="2";
+
+if(num1==num2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
---
+++ nullVsUndefined.js
@@ -0,0 +1,40 @@
+//1-undefined
+//let name;
+//console.log(name);
+
+
+//2-undefined
+//function add(num1,num2){
+    //sum=num1+num2;
+    //no return is given,so it will show undefined
+//}
+//const summation=add(10,20);
+//console.log(summation);
+
+
+
+//3-undefined
+// function add(num1,num2){
+//     sum=num1+num2;
+//     return sum;
+// }
+// const summation=add(10);//parameter value isn't given
+// console.log(summation);
+
+//eta solve kora jay default value diye
+
+
+
+
+
+//4-undefined
+// const person={name:"Jack William",age:20}
+// const {phone}=person;//phone toh kono oboject er characteristics-e na
+// console.log(phone);
+
+
+
+
+//5-undefined
+let name=undefined;
+console.log(name);
